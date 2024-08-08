af099d8981a6a06582ba915d888e3381b9afc41e
ramin <@rraahmad>
2024-08-08 19:18:37 -0400

    my new codes

--- src/testimg.java
+++ src/testimg.java
@@ -1,17 +1,28 @@
-
-
+import java.sql.SQLOutput;
 
 public class testimg {
 
     public static void main(String[] args) {
 
-        System.out.println("Ahmad khan ");
+int num1= 20;
+int num2=46;
+//int sum= num1+num2;
+        System.out.println(num1+num2);
+         System.out.println("ahamd khan ramin");
+         System.out.println("my name is ahamd khan ramin and i'm the tek school student and i wanna learn JAVA in here");
+
+        System.out.println("hey");
+        System.out.println("how u doing ");
+        System.out.println("how is the class going ");
+        System.out.println("and how is life ");
+        System.out.println("every thing will be ok ");
+
 
     }
 
 }
 
-
+//
 
 
 
